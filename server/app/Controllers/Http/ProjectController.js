92e105b1d2463a97e8866361d0eab6f1f7d2331e
dmitriy <dmitriy@coderiver.com.ua>
2018-07-08 20:36:54 +0300

    upd

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Project = use('App/Models/Project');
+const AuthorizationService = use('App/Services/AuthorizationService');
 
 class ProjectController {
     async index({ auth }) {
@@ -16,6 +17,23 @@ class ProjectController {
             title,
         });
         await user.projects().save(project);
+        return project;
+    }
+    async destroy({ auth, request, params }) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const project = await Project.find(id);
+        AuthorizationService.verifyPermission(project, user)
+        await project.delete();
+        return project;
+    }
+    async update({ auth, request, params }) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const project = await Project.find(id);
+        AuthorizationService.verifyPermission(project, user);
+        project.merge(request.only('title'));
+        await project.save();
         return project;
     }
 }
