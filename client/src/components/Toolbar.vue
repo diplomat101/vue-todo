e6cd7c104e7380431fa41ae0dc64f6d4928eb4ce
dmitriy <dmitriy@coderiver.com.ua>
2018-10-09 16:53:21 +0300

    fix loggedin state

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -3,7 +3,7 @@
     <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
     <v-toolbar-title class="mr-4">VUE TODO</v-toolbar-title>
     <v-toolbar-items>
-      <v-btn flat v-if="isLoggenIn">
+      <v-btn flat v-if="isLoggedIn">
           <v-icon class="mr-2">playlist_add_check</v-icon>
           Projects
       </v-btn>
@@ -11,15 +11,15 @@
     <v-spacer></v-spacer>
     <!-- <v-toolbar-items class="hidden-sm-and-down"> -->
     <v-toolbar-items>
-      <v-btn flat to="/register" v-if="!isLoggenIn">
+      <v-btn flat to="/register" v-if="!isLoggedIn">
           <v-icon class="mr-2">account_box</v-icon>
           Register
       </v-btn>
-      <v-btn flat v-if="!isLoggenIn">
+      <v-btn flat v-if="!isLoggedIn">
           <v-icon class="mr-2">fingerprint</v-icon>
           Login
       </v-btn>
-      <v-btn flat v-if="isLoggenIn">
+      <v-btn flat v-if="isLoggedIn">
           <v-icon class="mr-2">exit_to_app</v-icon>
           Logout
       </v-btn>
